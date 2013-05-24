20c6e5fb9e3961a9625d7edf4bf06053023b0614
Jason Edwards <jason.edwards@thelowry.com>
2013-05-24 16:17:15 +0100

    Moved files

---
+++ Source/Model/Email.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Description of Email
+ *
+ * @author edwardsj
+ */
+class Email {
+    public function construct()
+    {
+        
+    }
+    
+    private function sendEmail($email)
+    {
+        
+    }
+}
+
+?>
